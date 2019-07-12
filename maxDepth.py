b0447752898bbe33468b92d132422340f406056b
howard <bhowluo2@gmail.com>
2019-07-11 23:26:47 -0500

    max depth

---
+++ maxDepth.py
@@ -0,0 +1,9 @@
+def maxDepth(self, root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    if not root:
+        return 0
+    else:
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
